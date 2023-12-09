b045616b1d046ba2cc7b5079d96d401c5821f9fc
Abhishek Sinha <avishkarabhishek786@gmail.com>
2023-12-09 22:55:08 +0530

    updated mock tokens

--- contracts/mock/usdc.sol
+++ contracts/mock/usdc.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract USDCoin is ERC20, ERC20Burnable {
-    constructor(address initialOwner)
+    constructor()
         ERC20("USD Coin", "USDC")
     {}
 
--- contracts/mock/weth.sol
+++ contracts/mock/weth.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract USDCoin is ERC20, ERC20Burnable {
-    constructor(address initialOwner)
+contract WETH is ERC20, ERC20Burnable {
+    constructor()
         ERC20("Wrapped Ether", "WETH")
     {}
 
